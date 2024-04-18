3a6d2e1461297a702b73c144b485aaf04c55da22
Anand <anandharidas8921@gmail.com>
2024-04-18 14:10:09 +0530

    yes

--- app.js
+++ app.js
@@ -3,15 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var userRouter=require('./routes/user');
+var adminRouter=require('./routes/admin');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+
 
 var app = express();
-
+var exhbs=require('express-handlebars');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
+app.engine('hbs',exhbs.engine({extname:'hbs',defaultLayout:'layout',layoutsDir:__dirname+'/views/layouts/',partialsDir:__dirname+'/views/partials/'}));
+app.use('/',userRouter);
+app.use('/admin',adminRouter);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +25,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
